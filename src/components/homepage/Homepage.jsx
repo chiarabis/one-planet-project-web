df2d8e89909b74aa5dfce4c646b0adfefce6178e
Chiara <124071052+chiarabis@users.noreply.github.com>
2024-04-26 09:12:36 +0200

    Add files via upload

---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,29 @@
+import FirstBlock from './first block/FirstBlock';
+import SecondBlock from './second block/SecondBlock';
+import backgroundImage from '/2.jpg';
+import {motion} from 'framer-motion'
+
+
+export default function Homepage() {
+
+  return (
+    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}>
+
+    
+      <div className='bg-cover h-screen w-screen overflow-y-scroll overflow-x-hidden fixed flex flex-col items-center' style={{backgroundImage: `url(${backgroundImage})`}}>
+        
+        <div className='text-center my-20'>
+          <h1 className='anta-regular text-7xl glitch' data-text='One Planet Project'>One Planet Project</h1>
+        </div>
+        <FirstBlock/>
+        <SecondBlock/>
+        
+      </div>
+    </motion.div>
+    </>
+  )
+}

6f6514c531c52a204f18300c0be7af6d276ff250
Chiara <124071052+chiarabis@users.noreply.github.com>
2024-05-04 09:41:08 +0200

    Add files via upload

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Homepage from '../components/homepage/Homepage';
 import LocationSearched from '../components/topic/LocationSearched';
-import GlobalWarming from '../components/topic/GlobalWarming';
 import GlobalTemperature from '../components/topic/GlobalTemperature';
 import GreenhouseGases from '../components/topic/GreenhouseGases';
 import IceMelting from '../components/topic/IceMelting';
 import OceanWarming from '../components/topic/OceanWarming';
-import ActNow from '../components/homepage/homepage components/ActNow';
+import ForestLoss from '../components/topic/ForestLoss';
+import ActNow from '../components/topic/ActNow';
+
 
 
 
@@ -15,11 +16,11 @@ export default function Pages() {
     <Routes>
         <Route exact path='/' element={<Homepage/>}/>
         <Route path='/location-searched/:search' element={<LocationSearched/>}/>
-        <Route path='/global-warming' element={<GlobalWarming/>}/>
-        <Route path='/global-temperature' element={<GlobalTemperature/>}/>
         <Route path='/greenhouse-gases' element={<GreenhouseGases/>}/>
+        <Route path='/global-temperature' element={<GlobalTemperature/>}/>
         <Route path='/ice-melting' element={<IceMelting/>}/>
         <Route path='/ocean-warming' element={<OceanWarming/>}/>
+        <Route path='/forest-loss' element={<ForestLoss/>}/>
         <Route path='/act-now' element={<ActNow/>}/>
     </Routes>
   )

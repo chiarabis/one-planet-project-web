11fd8e725f017c7e16c58d14d2ce813fcac50d13
Chiara <124071052+chiarabis@users.noreply.github.com>
2024-05-24 17:34:11 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { BleepsProvider } from '@arwes/react';
+import { Helmet } from 'react-helmet'
 
 
 const bleepsSettings = {
@@ -34,6 +35,26 @@ function App() {
   return (
     <>
     <BrowserRouter>
+      <Helmet>
+        <meta charset="UTF-8" />
+        <title>One Planet Project</title>
+        <meta name="description" content="Stay informed and learn about CLIMATE CHANGE and its impacts" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="news, pollution, information, data, science, climate change, climate crisis, global warming, nasa, planet" />
+        <meta name="author" content="Chiara Bissolo" />
+        <meta property="og:title" content="One Planet Project" />
+        <meta property="og:description" content="One Planet Project - a web app for climate change" />
+        <meta property="og:image" content="/image-og.png" />
+        <meta property="og:url" content="" />
+        <meta property="og:type" content="website"/>
+        <link rel="canonical" href=""/>
+        <link rel="icon" type="image/png" href="/favicon/website-favicon.ico"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+Expanded&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Anta&family=Encode+Sans+Expanded&display=swap" rel="stylesheet" />
+      </Helmet>
+
       <BleepsProvider {...bleepsSettings}>
       
         {loading ? <Loader/> : <Pages/>}

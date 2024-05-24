39dcd6a29e3e2521a14047e6a1faa2f0edac6b4c
Chiara <124071052+chiarabis@users.noreply.github.com>
2024-05-24 12:41:03 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,9 @@ export default {
         'white': 'rgb(250, 250, 250)',
         'neon-cyan': '#00ffe2',
         'neon-cyan-shadow': '#00ffe230',
-        'neon-green': '#96ff00',
         'neon-pink': '#ff00c1',
         'dark': 'rgb(17, 24, 39)',
-        'red': 'rgb(239, 68, 68)',
-        'red-shadow': 'rgba(239, 68, 68, 0.6)',
+        'red': '#b91c1c',
       }
     },
   },
